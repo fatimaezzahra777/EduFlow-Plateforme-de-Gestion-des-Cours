042823ad5daf79ee41138f5a07d2889b6e7751b7
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-31 11:41:09 +0100

    ajoute des modifications dans les controllers

--- app/Http/Controllers/InterestController.php
+++ app/Http/Controllers/InterestController.php
@@ -4,20 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Interest;
+use App\Services\InterestService;
 
 class InterestController extends Controller
 {
+    public function __construct(private InterestService $interestService)
+    {
+    }
+
     public function index()
     {
-        return Interest::all();
+        return response()->json($this->interestService->all());
     }
 
     public function store(Request $request)
     {
-        $interest = Interest::create([
-            'name' => $request->nom
-        ]);
-
-        return response()->json($interest);
+        return response()->json(
+            $this->interestService->create($request->nom),
+            201
+        );
     }
 }
