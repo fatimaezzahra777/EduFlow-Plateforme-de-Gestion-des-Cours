2d1e65ab9a96f8844dcac12136c17c33dbcc9373
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-31 11:43:12 +0100

    ajoute des modifications dans les controllers

--- app/Http/Controllers/StatistiqueController.php
+++ app/Http/Controllers/StatistiqueController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\CourseService;
 
 class StatistiqueController extends Controller
 {
-    //
+    public function __construct(private CourseService $courseService)
+    {
+    }
+
+    public function show($courseId)
+    {
+        return response()->json(
+            $this->courseService->statisticsForTeacher($courseId)
+        );
+    }
 }
